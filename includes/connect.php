f6378e6b39656f7efc30e3db82d90887dc9ae1de
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2023-09-24 05:19:42 +0300

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '36987412');
-define('DB_DATABASE', 'tyres');
+define('DB_USERNAME', 'u640333703_tyresell');
+define('DB_PASSWORD', 'p>2C!tD8+9U');
+define('DB_DATABASE', 'u640333703_tyresell');
 
 $pdo = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
